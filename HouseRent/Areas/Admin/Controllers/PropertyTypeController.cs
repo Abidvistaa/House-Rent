50ada33819de84642f2f19bf5a519ba6eb7e6a48
Abid Hassan <abidhassandiba@gmail.com>
2021-06-26 21:20:11 +0600

    Section2: CRUD PropertyType

---
+++ HouseRent/Areas/Admin/Controllers/PropertyTypeController.cs
@@ -0,0 +1,79 @@
+﻿using HouseRent.Data;
+using HouseRent.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseRent.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PropertyTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public PropertyTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            var propertyTypes = _db.PropertyTypes.ToList();
+            return Json(new { data = propertyTypes });
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var type=_db.PropertyTypes.Find(id);
+            return View(type);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PropertyType propertyType)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.PropertyTypes.Add(propertyType);
+                await _db.SaveChangesAsync();
+            }
+            TempData["save"] = "Created successfully";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(PropertyType propertyType)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.PropertyTypes.Update(propertyType);
+                await _db.SaveChangesAsync();
+            }
+            TempData["edit"] = "Updated successfully";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+
+        public ActionResult Delete(int id)
+        {
+            var type = _db.PropertyTypes.Where(x => x.Id == id).FirstOrDefault<PropertyType>();
+            _db.Remove(type);
+            _db.SaveChanges();
+            return Json(new { success=true,message="Deleted Successfully"});
+        }
+    }
+}
---
+++ HouseRent/wwwroot/js/propertyType.js
@@ -0,0 +1,59 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/PropertyType/GetAll"
+
+        },
+        "columns": [
+            { "data": "propType" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/PropertyType/Edit/${data}" class="btn btn-success text-white style" style="cursor:pointer">
+                                    <i class="far fa-edit"></i> 
+                                </a>
+                                
+                                <a onclick=Delete("/Admin/PropertyType/Delete/${data}") class="btn btn-danger text-white style" style="cursor:pointer">
+                                    <i class="far fa-trash-alt"></i>
+                                </a>
+                            </div>
+                            `;
+                }, "width": "60%"
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure want to delete?",
+        text: "You will not be able to restore the data",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
+
