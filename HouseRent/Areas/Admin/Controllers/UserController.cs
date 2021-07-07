2cb5a44815041fc20905e8f34d2bc23e030c490a
Abid Hassan <abidhassandiba@gmail.com>
2021-07-07 23:36:34 +0600

    Section17: User CRUD

--- HouseRent/Areas/Admin/Controllers/UserController.cs
+++ HouseRent/Areas/Admin/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using HouseRent.Models;
+﻿using HouseRent.Data;
+using HouseRent.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,31 +13,84 @@ namespace HouseRent.Areas.Admin.Controllers
     public class UserController : Controller
     {
         UserManager<IdentityUser>_userManager;
-        public UserController(UserManager<IdentityUser> userManager)
+        ApplicationDbContext _db;
+        public UserController(UserManager<IdentityUser> userManager, ApplicationDbContext db)
         {
             _userManager = userManager;
+            _db = db;
         }
         public IActionResult Index()
         {
             return View();
         }
-
-        public async Task<IActionResult> Create()
+        public ActionResult GetAll()
+        {
+            List<ApplicationUser> applicationUsers = _db.ApplicationUsers.ToList<ApplicationUser>();
+            return Json(new { data = applicationUsers });
+        }
+        public  IActionResult Create()
         {
 
             return View();
         }
+        public async Task<ActionResult> Edit(string id)
+        {
+            var user = await _db.ApplicationUsers.FindAsync(id);
+            return View(user);
+        }
+
 
         [HttpPost]
         public async Task<IActionResult> Create(ApplicationUser applicationUser)
         {
             if (ModelState.IsValid)
             {
-                var result = await _userManager.CreateAsync(applicationUser);
-                TempData["save"] = "User has been created Successfully";
-                return RedirectToAction("Index", "Home", new { area = "Customer" });  
+                var result = await _userManager.CreateAsync(applicationUser,applicationUser.PasswordHash);
+                if (result.Succeeded) {
+                    TempData["save"] = "User has been created Successfully";
+                    return RedirectToAction(nameof(Index));
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
             }
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var userInfo = _db.ApplicationUsers.FirstOrDefault(x => x.Id == user.Id);
+                userInfo.FirstName = user.FirstName;
+                userInfo.LastName = user.LastName;
+                userInfo.UserName = user.UserName;
+                var result = await _userManager.UpdateAsync(userInfo);
+                if (result.Succeeded)
+                {
+                    TempData["edit"] = "User has been Updated";
+                    return RedirectToAction(nameof(Index));
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            var user = _db.ApplicationUsers.Where(x => x.Id == id).FirstOrDefault<ApplicationUser>();
+            _db.Remove(user);
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Deleted Successfully" });
+        }
+
     }
 }
---
+++ HouseRent/wwwroot/js/user.js
@@ -0,0 +1,62 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/User/GetAll"
+
+        },
+        "columns": [
+            { "data": "firstName" },
+            { "data": "lastName" },
+            { "data": "userName"},
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/User/Edit/${data}" class="btn btn-success text-white style" style="cursor:pointer">
+                                    <i class="far fa-edit"></i> 
+                                </a>
+                                <a onclick=Delete("/Admin/User/Delete/${data}") class="btn btn-danger text-white style" style="cursor:pointer">
+                                    <i class="far fa-trash-alt"></i>
+                                </a>
+                            </div>
+                            `;
+                }
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure want to delete?",
+        text: "You will not be able to restore the data",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+} 
+
+
+//Alertify
